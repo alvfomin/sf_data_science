48a86153b27a28506993f13eafd2e04e4572feef
alvfomin <146915967+alvfomin@users.noreply.github.com>
2023-10-04 09:07:57 +0300

    Update module_3.py

--- project_0/module_3.py
+++ project_0/module_3.py
@@ -1,1 +1,25 @@
+import numpy as np
+import random
 
+# Фиксируем начальное значение seed для воспроизводимости
+seed = 42
+random.seed(seed)
+
+number = np.random.randint(1, 101) # загадываем число
+
+# количество попыток
+count = 0
+low = 1
+high = 100
+
+while True:
+    count += 1
+    predict_number = (low + high) // 2
+    
+    if predict_number == number:
+        print(f"Вы угадали число! Это число = {number}, за {count} попыток")
+        break
+    elif predict_number < number:
+        low = predict_number + 1 # Число должно быть больше
+    else:
+        high = predict_number - 1 # Число должно быть меньше
